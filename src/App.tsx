b46a555c99ab4c10ee29e5bd4b51ec21a4c8c5f5
Mohammad Shahbaz Alam <shahbaz17@users.noreply.github.com>
2024-08-10 10:19:24 +0530

    Add Web3Auth + WorldCoin
    * Added Web3Auth support
    * Added Sign in with WorldCoin
    * IDKit to verify the proof

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,197 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
+import { Web3AuthNoModal } from "@web3auth/no-modal";
+import {
+  WALLET_ADAPTERS,
+  CHAIN_NAMESPACES,
+  IProvider,
+  WEB3AUTH_NETWORK,
+  UX_MODE,
+} from "@web3auth/base";
+import { OpenloginAdapter } from "@web3auth/openlogin-adapter";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { IDKitWidget, VerificationLevel } from "@worldcoin/idkit";
+import Logo from "./assets/super.connect-logo.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [web3auth, setWeb3auth] = useState<Web3AuthNoModal | null>(null);
+  const [, setProvider] = useState<IProvider | null>(null);
+  const [loggedIn, setLoggedIn] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const chainConfig = {
+          chainNamespace: CHAIN_NAMESPACES.EIP155,
+          chainId: "0x1", // Please use 0x1 for Mainnet
+          rpcTarget: "https://rpc.ankr.com/eth",
+          displayName: "Ethereum Mainnet",
+          blockExplorerUrl: "https://etherscan.io/",
+          ticker: "ETH",
+          tickerName: "Ethereum",
+          logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+        };
+
+        const privateKeyProvider = new EthereumPrivateKeyProvider({
+          config: { chainConfig },
+        });
+
+        const web3auth = new Web3AuthNoModal({
+          clientId:
+            "BPi5PB_UiIZ-cPz1GtV5i1I2iOSOHuimiXBI0e-Oe_u6X3oVAbCiAZOTEBtTXw4tsluTITPqA8zMsfxIKMjiqNQ",
+          web3AuthNetwork: WEB3AUTH_NETWORK.SAPPHIRE_MAINNET,
+          privateKeyProvider,
+        });
+
+        const openloginAdapter = new OpenloginAdapter({
+          adapterSettings: {
+            uxMode: UX_MODE.REDIRECT,
+            loginConfig: {
+              jwt: {
+                verifier: "w3a-worldcoin-demo",
+                typeOfLogin: "jwt",
+                clientId: "7u5jfJ3bakrfLBJYhyTquohpOth0Tmt7",
+              },
+            },
+          },
+        });
+        web3auth.configureAdapter(openloginAdapter);
+        setWeb3auth(web3auth);
+
+        await web3auth.init();
+        setProvider(web3auth.provider);
+
+        if (web3auth.connected) {
+          setLoggedIn(true);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    init();
+  }, []);
+
+  const uiConsole = (...args: unknown[]) => {
+    const el = document.querySelector("#console>p");
+    if (el) {
+      el.innerHTML = JSON.stringify(args || {}, null, 2);
+    }
+  };
+
+  const verifyProof = async (proof: unknown) => {
+    console.log("proof", proof);
+    const response = await fetch(
+      "https://worldcoin-verify-proof-server.vercel.app/api/verify",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(proof),
+      }
+    );
+    if (response.ok) {
+      const { verified } = await response.json();
+      return verified;
+    } else {
+      const { code, detail } = await response.json();
+      throw new Error(`Error Code ${code}: ${detail}`);
+    }
+  };
+
+  const onSuccess = async () => {
+    console.log("Success");
+    await getUserInfo();
+  };
+
+  const login = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const web3authProvider = await web3auth.connectTo(
+      WALLET_ADAPTERS.OPENLOGIN,
+      {
+        loginProvider: "jwt",
+        extraLoginOptions: {
+          domain: "https://web3auth.jp.auth0.com",
+          verifierIdField: "sub",
+          connection: "worldcoin",
+        },
+      }
+    );
+    setProvider(web3authProvider);
+  };
+
+  const logout = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    await web3auth.logout();
+    setLoggedIn(false);
+    setProvider(null);
+  };
+
+  const getUserInfo = async () => {
+    if (!web3auth) {
+      uiConsole("web3auth not initialized yet");
+      return;
+    }
+    const user = await web3auth.getUserInfo();
+    uiConsole(user);
+  };
+
+  // View
+  const loggedInView = (
+    <>
+      <div className="flex-container">
+        <div>
+          <button onClick={getUserInfo} className="card">
+            Get User Info
+          </button>
+        </div>
+        <div>
+          <button onClick={logout} className="card">
+            Log Out
+          </button>
+        </div>
+        <div>
+          <IDKitWidget
+            app_id="app_staging_f3a97c0c7a87ffad90737c4cd149a763"
+            action="verify-for-bigger-transaction"
+            verification_level={VerificationLevel.Device}
+            handleVerify={verifyProof}
+            onSuccess={onSuccess}
+          >
+            {({ open }) => (
+              <button onClick={open} className="card">
+                Verify with World ID
+              </button>
+            )}
+          </IDKitWidget>
+        </div>
+      </div>
+      <div id="console" style={{ whiteSpace: "pre-line" }}>
+        <p style={{ whiteSpace: "pre-line" }}>Logged in Successfully!</p>
+      </div>
+    </>
+  );
+
+  const loggedOutView = (
+    <button onClick={login} className="card">
+      Sign in with World ID
+    </button>
+  );
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={Logo} className="logo" alt="Super Connect logo" />
       </div>
       <h1>Super Connect</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <div className="grid">{loggedIn ? loggedInView : loggedOutView}</div>
       <p className="read-the-docs">
         <a
           href="https://github.com/SuperConnectHack/SuperConnect"
